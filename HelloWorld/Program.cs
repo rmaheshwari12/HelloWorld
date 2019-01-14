648b5ae207308ffe15cbfaf314b166005ac69be1
Rushikesh Maheshwari <rushikesh.maheshwari12@outlook.com>
2019-01-14 15:24:07 -0500

    changes1

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,7 +6,10 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            /* This is how you put test comments*/
+            Console.WriteLine("Press Any key to continue...");
             Console.WriteLine("Hello World!");
+            Console.ReadKey(true);
         }
     }
 }
