15795d089393914bc13b760115d807d94a033475
Rushikesh Maheshwari <rushikesh.maheshwari12@outlook.com>
2019-01-29 21:11:50 -0500

    rushi1

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -12,6 +12,7 @@ namespace HelloWorld
             Console.WriteLine("Hello World from Rushikesh!");
             Console.WriteLine("Hello World from Rashmi!");
             Console.WriteLine("Hello World from Rash again!");
+            Console.WriteLine("Hello World from Rushikesh Again, because need to check the sync");
             Console.WriteLine("Press Any key to continue...");
             
             Console.ReadKey(true);
